b1e48456fce3d337a0b5b4b5bcdbc306e7f6a383
hongbin-yu <hongbin.yu413@gmail.com>
2018-03-18 19:13:00 -0400

    add media to youchat

--- smarti-cloud/src/main/webapp/resources/js/pageContent.js
+++ smarti-cloud/src/main/webapp/resources/js/pageContent.js
@@ -293,7 +293,7 @@ function syncChat() {
 				    if(c.path.indexOf("/chat/"+c.createdBy)>=0) {
 					    html = '<div id="'+c.uid+'" class="panel panel-default"><header class="panel-heading">';
 						html +='<h5 class="panel-title">'+c.createdBy+' <span class="small text-left">'+cDate.toISOString()+'</span><a href="javascript:removeTag('+"'"+c.uid+"'"+')"><button title="\u70B9\u51FB\u5220\u9664" class="btn btn-warning btn-xs pull-right"><span class="glyphicon glyphicon-trash"></span></button></a></h5>';
-						html +='</header><div class="panel-body"><img class=\"img-responsive pull-left\" src=\"'+c.icon+"\">"+c.content+'</div></div><div class="clearfix"></div>';
+						html +='</header><div class="panel-body"><img class=\"img-responsive pull-left\" src=\"'+c.icon+"\">"+c.content+'</div></div></div><div class="clearfix"></div>';
 
 				    }else {
 					    html = '<div id="'+c.uid+'" class="panel panel-success"><header class="panel-heading">';
