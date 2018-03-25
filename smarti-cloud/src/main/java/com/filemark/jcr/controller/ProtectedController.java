45c361e3ee50796fda22d2c13528c1ec48cca129
hongbin-yu <hongbin.yu413@gmail.com>
2018-03-24 21:22:00 -0400

    add media to youchat

--- smarti-cloud/src/main/java/com/filemark/jcr/controller/ProtectedController.java
+++ smarti-cloud/src/main/java/com/filemark/jcr/controller/ProtectedController.java
@@ -72,7 +72,10 @@ public class ProtectedController extends BaseController {
    		User user = (User)jcrService.getObject("/system/users/"+username);
    		String chatRoot = "/chat";
    		if(path==null && user != null && "Owner".equals(user.getRole())) path="/chat";
-
+   		if(!jcrService.nodeExsits("/"+username+"/assets/ypuchat")) {
+   			jcrService.addNodes("/"+username+"/assets/ypuchat", "nt:unstructured", username);
+   			
+   		}
 		String folderQuery = "select s.* from [nt:base] AS s INNER JOIN [nt:base] AS child ON ISCHILDNODE(child,s) WHERE ISDESCENDANTNODE(s,["+chatRoot+"])" +" and child.userName like '%"+username+"' and s.ocm_classname='com.filemark.jcr.model.Folder' and child.ocm_classname ='com.filemark.jcr.model.User' order by s.path";
 
    		if(user.getRole().equals("Administrator")||user.getRole().equals("Owner"))
