541312772808e0b114e7ba5b6ffff4e80d33294f
hongbin-yu <hongbin.yu413@gmail.com>
2018-03-29 19:15:18 -0400

    ip2geo

--- smarti-cloud/src/main/java/com/filemark/jcr/serviceImpl/IP2GeoLocationService.java
+++ smarti-cloud/src/main/java/com/filemark/jcr/serviceImpl/IP2GeoLocationService.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.filemark.jcr.model.Asset;
 import com.filemark.jcr.model.GeoIP;
 import com.maxmind.geoip2.DatabaseReader;
@@ -13,10 +16,15 @@ import com.maxmind.geoip2.model.CityResponse;
 public class IP2GeoLocationService {
     private DatabaseReader dbReader;
     private String dbLcation=Asset.getDevicePath()+"/database/GeoLite2-City_20180327/GeoLite2-City.mmdb";
+	private final Logger log = LoggerFactory.getLogger(IP2GeoLocationService.class);
     
-    public IP2GeoLocationService() throws IOException {
-        File database = new File(dbLcation);
-        dbReader = new DatabaseReader.Builder(database).build();
+    public IP2GeoLocationService(){
+    	try {
+	        File database = new File(dbLcation);
+	        dbReader = new DatabaseReader.Builder(database).build();
+    	}catch(IOException e) {
+    		log.error(e.getMessage());
+    	}
     }
      
     public GeoIP getLocation(String ip) 
