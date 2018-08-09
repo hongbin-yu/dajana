dce67c54134bbcdc8b54e298a6f75fb5c6a9b00f
hongbin-yu <hongbin.yu413@gmail.com>
2018-08-09 18:52:59 -0400

    folder upload

--- smarti-cloud/src/main/webapp/resources/js/djn.js
+++ smarti-cloud/src/main/webapp/resources/js/djn.js
@@ -857,7 +857,8 @@ function sendFormData(formData,file) {
 				    speed = fileSize*8/(end.getTime() - start.getTime());
 	                selDiv.innerHTML ="<section id=\""+file.name+"\"><h5>"+file.name+"("+(speed/1000).toFixed(2)+" MB/s)</h5><progress class=\"full-width\" value=\""+fileSize +"\" max=\""+fileSize+"\"><span class=\"wb-inv\">"+100+"%</span></progress></section>";
 		    		selDiv.innerHTML += "<section class=\"alert alert-success\"><h3 class=\"5\">"+(i)+"/"+total+i18n("document_uploaded")+i18n("success")+"</h3></section>";
-		    		output(data);
+		    		if(i < 100)
+		    			output(data);
 
 		    	}
 		    },
@@ -877,7 +878,8 @@ function getAsset(formData,file) {
 	    type: "GET",
 	    success: function(data) {
 	    	if(data.uid) {
-	    		output(data);
+	    		if(i <= 12)
+	    			output(data);
 	    		percentComplete =1000;
 	    		selDiv.innerHTML = "<section class=\"alert alert-success\"><h3 class=\"5\">"+(i)+"/"+total+i18n("document_uploaded")+i18n("success")+"</h3></section>";
 	    		//i++;
