02846bc7b303e85bbf6ac537e89241096f242ad8
hongbin-yu <hongbin.yu413@gmail.com>
2018-08-21 22:59:01 -0400

    gps

--- smarti-cloud/src/main/webapp/resources/js/djn.js
+++ smarti-cloud/src/main/webapp/resources/js/djn.js
@@ -903,7 +903,7 @@ function sendFormData(formData,file) {
 				    speed = fileSize*8/(end.getTime() - start.getTime());
 	                selDiv.innerHTML ="<section id=\""+file.name+ " ("+i+"/"+total+")\"><h5>"+file.name+"("+(speed/1000).toFixed(2)+" MB/s)</h5><progress class=\"full-width\" value=\""+fileSize +"\" max=\""+fileSize+"\"><span class=\"wb-inv\">"+100+"%</span></progress></section>";
 		    		selDiv.innerHTML += "<section class=\"alert alert-success\"><h3 class=\"5\">"+(i)+"/"+total+i18n("document_uploaded")+i18n("success")+"</h3></section>";
-		    		//if(i < 24)
+		    		if(i < 100)
 		    			output(data);
 /*			    	if(i==total || total==0) {
 		    			setTimeout(function () {
