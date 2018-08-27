31542eb92dd9f292e84fac26b88fbd68a6fa690b
hongbin-yu <hongbin.yu413@gmail.com>
2018-08-26 20:55:33 -0400

    gps position

--- smarti-cloud/src/main/java/com/filemark/jcr/controller/SiteController.java
+++ smarti-cloud/src/main/java/com/filemark/jcr/controller/SiteController.java
@@ -1352,11 +1352,11 @@ public class SiteController extends BaseController {
 	           				jcrService.addOrUpdate(asset);
 	           				if(!"1080x720".equals(resolution) && !contentType.equals("video/mp4"))
 	           					ImageUtil.video2mp4(file.getAbsolutePath(),resolution);
-	           				if(asset.getContentType().startsWith("image/")) {
-	           					jcrService.autoRoateImage(path);
-	           					//jcrService.createIcon(path, 400, 400);
-	           				}
-	        			}        				
+	        			} 
+           				if(asset.getContentType().startsWith("image/")) {
+           					jcrService.autoRoateImage(path);
+           					//jcrService.createIcon(path, 400, 400);
+           				}
 
         			}else {
         				//logger.debug("Writing jcr");
