5af38714251f8327b2fb2d8de27961edd38bb4e3
hongbin-yu <hongbin.yu413@gmail.com>
2019-01-08 16:21:38 -0500

    onMessage

--- smarti-cloud/src/main/webapp/resources/chat/main.js
+++ smarti-cloud/src/main/webapp/resources/chat/main.js
@@ -45,11 +45,41 @@ function onConnect(status)
 	log('Strophe is disconnecting.');
     } else if (status == Strophe.Status.DISCONNECTED) {
 	log('Strophe is disconnected.');
+	connection.addHandler(onMessage, null, 'message', null, null,  null); 
+	connection.send($pres().tree());	
 	$('#connect').get(0).value = 'connect';
     } else if (status == Strophe.Status.CONNECTED) {
 	log('Strophe is connected.');
-	connection.disconnect();
+	//connection.disconnect();
+    } else if (status == Strophe.Status.ATTACHED) {
+         log('Strophe is attached.');
+         var button = $('#connect').get(0);
+         button.value = 'disconnect';
+         $(button).show();
+     }
+}
+function onMessage(msg) {
+    var to = msg.getAttribute('to');
+    var from = msg.getAttribute('from');
+    var type = msg.getAttribute('type');
+    var elems = msg.getElementsByTagName('body');
+
+    if (type == "chat" && elems.length > 0) {
+	var body = elems[0];
+
+	log('ECHOBOT: I got a message from ' + from + ': ' + 
+	    Strophe.getText(body));
+    
+	var reply = $msg({to: from, from: to, type: 'chat'})
+            .cnode(Strophe.copyElement(body));
+	connection.send(reply.tree());
+
+	log('ECHOBOT: I sent ' + from + ': ' + Strophe.getText(body));
     }
+
+    // we must return true to keep the handler alive.  
+    // returning false would remove it after it finishes.
+    return true;
 }
 
 $(document).ready(function () {
